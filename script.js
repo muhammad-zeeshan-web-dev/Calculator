34fa35bc16e064df05e896c2fa5033cc637c4087
Muhammad Zeeshan <muhammad.zeeshan8907@gmail.com>
2026-09-24 17:35:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Caculator Program
+
+const display = document.getElementById("display");
+
+function appendtoDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculator() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch (error) {
+        display.value = "Error";
+    }
+}
